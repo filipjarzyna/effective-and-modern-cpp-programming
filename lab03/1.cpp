9b9520931df7a49e3a0a544fa293b3a9cf422e02
filipjarzyna <filipjarzyna@protonmail.com>
2026-03-23 11:53:48 +0100

    ex1 done

--- lab03/1.cpp
+++ lab03/1.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+typedef double(*F1)(int, double);
+typedef double(*F2)(double, double);
+typedef double(*F3)(int, double, double, F1, F2) ;
+
 /// Computes n-th root of x
 double sqrtn(int n, double x){
-	return exp( log(x) / n); 
+  return exp( log(x) / n); 
 }
 double power(double  x, double y){
-	return exp( log(x) * y);
+  return exp( log(x) * y);
 }
 template <typename F, typename G>
 double function(int n, double y, double z, F f, G g){
-	return (f(n,y) > z) ? g(z, y) : g(y, z);
+  return (f(n,y) > z) ? g(z, y) : g(y, z);
 }
 int main(){
-	auto fp1 = sqrtn;
-  auto fp2 = power;
-	auto fp3 = function<decltype(fp1), decltype(fp2)>;
-	cout << fp3(2, 10, 3, fp1, fp2) << endl;
-	cout << fp3(3, 10, 3, fp1, fp2) << endl;
-	return 0;
+  F1 fp1 = sqrtn;
+  F2 fp2 = power;
+  F3 fp3 = function<F1, F2>;
+  cout << fp3(2, 10, 3, fp1, fp2) << endl;
+  cout << fp3(3, 10, 3, fp1, fp2) << endl;
+  return 0;
 }
