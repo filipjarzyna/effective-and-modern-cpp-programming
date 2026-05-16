c6fdcd225ab74f5ada8efb071ae25557b6c39158
filipjarzyna <filipjarzyna@protonmail.com>
2026-05-16 18:32:39 +0200

    lab10 done

--- lab10/2.cpp
+++ lab10/2.cpp
@@ -1,49 +1,68 @@
+#include <algorithm>
 #include <iostream>
-#include <chrono>
-#include <string>
+#include <iterator>
+#include <list>
+#include <forward_list>
+#include <vector>
+#include <deque>
 
-class Timer{
-   std::string name;
+template <typename Container>
+double median(Container c);
 
-public:
-   Timer(const std::string & name) : name(name){
-   }
-   ~Timer(){
-       std::cout << name << " : " << 0 << " ms. " << std::endl;
-   }
-   auto durationInNanoseconds(){
-       return 0;
-   }
-};
+template <typename Container>
+double median_impl(Container& c, std::random_access_iterator_tag) {
+    using std::begin;
+    using std::end;
 
-long fibonacci(unsigned n){
-    if (n < 2) return n;
-    return fibonacci(n-1) + fibonacci(n-2);
-}
+    const size_t n = c.size();
+    auto mid = c.begin() + n / 2;
 
-int main()
-{
-    Timer total("Total time");
-    for(auto n : {1, 5, 15, 20, 25, 40, 45}){
-        {
-            Timer t("fibonacci(" + std::to_string(n) + ")");  // RAII
-            fibonacci(n);
+    std::nth_element(c.begin(), mid, c.end());
+    double upper = *mid;
 
-        }
-    //2
-    //3
+    if (n % 2 == 1) {
+        return upper;
     }
-    std::cout << "Elapsed " << total.durationInNanoseconds() << " ns.\n";
-}
-/** Expected output similar to:
 
-fibonacci(1) : 0.000619 ms.
-fibonacci(5) : 0.000891 ms.
-fibonacci(15) : 0.01025 ms.
-fibonacci(20) : 0.094905 ms.
-fibonacci(25) : 1.04443 ms.
-fibonacci(40) : 989.97 ms.
-fibonacci(45) : 8537.47 ms.
-Elapsed 9528821533 ns.
-Total time : 9528.84 ms.
-*/
+    auto left_max = std::max_element(c.begin(), mid);
+    double lower = *left_max;
+
+    return (lower + upper) / 2.0;
+}
+
+template <typename Container>
+double median_impl(Container& c, std::forward_iterator_tag) {
+    using std::begin;
+    using std::end;
+
+    c.sort();
+
+    const size_t n = std::distance(c.begin(), c.end());
+    auto mid = c.begin();
+    std::advance(mid, n / 2);
+
+    if (n % 2 == 1) {
+        return static_cast<double>(*mid);
+    }
+
+    auto prev = mid;
+    --prev;
+
+    return (*prev + *mid) / 2.0;
+}
+
+template <typename Container>
+double median(Container c) {
+    using category = typename std::iterator_traits<
+        typename Container::iterator
+    >::iterator_category;
+
+    return median_impl(c, category{});
+}
+
+int main() {
+    std::list<int> a{3, 2, 5, 1, 4};
+    std::cout << median(a) << std::endl; // 3
+    std::vector<int> v{3, 1, 4, 2};
+    std::cout << median(v) << std::endl; // 2.5
+}
